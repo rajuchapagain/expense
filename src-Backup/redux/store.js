1799cebb3fe94e1dc469b61fd4f364f761e2614d
Raju Chapagain <raju.chapagain@outlook.com>
2021-09-26 20:00:09 +1000

    removed the slice project, will include in new project file

--- src-Backup/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ExpensesSlice from "./expensesSlice";
-import ToastMiddleware from "../middlewares/ToastMiddleware";
-export const store = configureStore({
-  reducer: {
-    expensesSlice: ExpensesSlice,
-  },
-  middleware: [ToastMiddleware],
-});
